4b431531786ca6eaf4a3e35a8a8a93ae5a0119c9
Jack Hou <jack.hou@Live.com>
2014-06-25 12:11:55 -0700

    Change Home Page Interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Jmo::Application.routes.draw do
   resources :voices
 
   devise_for :users
-  root "pages#home"
+  root "voices#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
